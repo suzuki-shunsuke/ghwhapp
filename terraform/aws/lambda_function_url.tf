dcd30cc040d7f7d2d386b724fd91ed6b4504bfc9
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2026-04-26 12:14:53 +0900

    chore: remove Terraform example

--- terraform/aws/lambda_function_url.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_lambda_function_url" "main" {
-  count              = local.use_api_gateway ? 0 : 1
-  function_name      = aws_lambda_function.main.function_name
-  authorization_type = "NONE"
-}
